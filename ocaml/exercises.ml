19328ab135f15d5af298099560fdeab826e5b69f
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-10-31 12:40:32 -0700

    completed binary search tree ocaml with z

--- ocaml/exercises.ml
+++ ocaml/exercises.ml
@@ -32,4 +32,37 @@ let surface_area sa =
   | Sphere r -> 4. *. Float.pi *. (r ** 2.)
   | Box (l, w, h) -> 2. *. (l *. w +. l *. h +. w *. h);;
 
-(* Write your binary search tree implementation here *)
+type 'a binary_search_tree = 
+  | Empty 
+  | Node of 'a binary_search_tree * 'a * 'a binary_search_tree
+
+let rec insert value tree = 
+  match tree with 
+  | Empty -> Node (Empty, value, Empty)
+  | Node (left, v, right) -> 
+    if value < v then
+      Node (insert value left, v, right)
+    else
+      Node (left, v, insert value right);;
+      
+
+let rec size tree = 
+  match tree with 
+  | Empty -> 0
+  | Node (left, _, right) -> 1 + size left + size right;;
+
+let rec contains value tree =
+  match tree with 
+  | Empty -> false
+  | Node (left, v, right) ->
+    if value = v then 
+      true
+    else if value < v then 
+      contains value left
+    else
+      contains value right;;
+
+let rec inorder tree = 
+  match tree with 
+  | Empty -> []
+  | Node (left, v, right) -> inorder left @ [v] @ inorder right;;
--- ocaml/exercises_test.ml
+++ ocaml/exercises_test.ml
@@ -76,7 +76,6 @@ let s2 = Box (3.0, 4.0, 5.0) in (
   expect (surface_area s2 = 94.0);
 );;
 
-(*
 suite "binary search tree";;
 let t1 = Empty in
 let t2 = insert 5 t1 in
@@ -99,6 +98,5 @@ let t5 = insert 4 t4 in (
   expect (inorder t4 = [3; 5; 7]);
   expect (inorder t5 = [3; 4; 5; 7]);
 );;
-*)
 
 Printf.printf "\n%d passed, %d failed\n" !passes !failures;;
