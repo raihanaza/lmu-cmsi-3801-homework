1b3b9d7c0b2915fe76b70cdf527c8c2f5e44b919
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-11-21 10:16:24 -0800

    adding private reallocate method

--- cpp/stack.h
+++ cpp/stack.h
@@ -1,29 +1,13 @@
-// A class for an expandable stack. There is already a stack class in the
-// Standard C++ Library; this class serves as an exercise for students to
-// learn the mechanics of building generic, expandable, data structures
-// from scratch with smart pointers.
-
 #include <stdexcept>
 #include <string>
 #include <memory>
 using namespace std;
-
-// A stack object wraps a low-level array indexed from 0 to capacity-1 where
-// the bottommost element (if it exists) will be in slot 0. The member top is
-// the index of the slot above the top element, i.e. the next available slot
-// that an element can go into. Therefore if top==0 the stack is empty and
-// if top==capacity it needs to be expanded before pushing another element.
-// However for security there is still a super maximum capacity that cannot
-// be exceeded.
 
 #define MAX_CAPACITY 32768
 #define INITIAL_CAPACITY 16
 
 template <typename T>
 class Stack {
-  // Add three fields: elements, a smart pointer to the array of elements,
-  // capacity, the current capacity of the array, and top, the index of the
-  // next available slot in the array.
   unique_ptr<T[]> elements;
   int capacity;
   int top;
@@ -50,19 +34,16 @@ public:
     return top == MAX_CAPACITY;
   }
 
-  // Write your push() method here
   void push(T item) {
     if (top == MAX_CAPACITY) {
       throw overflow_error("Stack has reached maximum capacity");
     }
     if (top == capacity) {
-      // call your reallocate here
       reallocate(2 * capacity);
     }
     elements[top++] = item;
   }
 
-  // Write your pop() method here
   T pop() {
     if (is_empty()) {
       throw underflow_error("cannot pop from empty stack");
@@ -72,9 +53,6 @@ public:
     if (top > 0 && top <= capacity / 4) {
       reallocate(capacity / 2);
     }
-    // get the top element, pop it
-    // overwrite the top element with default value (for security)
-    // return top element
     return popped;
   }
 
