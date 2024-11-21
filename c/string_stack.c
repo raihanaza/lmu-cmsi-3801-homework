47f20ec3dc5dbb210216d8848337347279ea4c48
ceciliazaragoza <czarago3@lion.lmu.edu>
2024-11-21 13:51:56 -0800

    adding updated c destroy function

--- c/string_stack.c
+++ c/string_stack.c
@@ -1,10 +1,11 @@
 #include "string_stack.h"
+#include <stdlib.h>
+#include <string.h>
 
-// Complete your string stack implementation in this file.
+#define INITIAL_CAPACITY 16
 // TODO: is_full function for CPP and size, is_empty, and destroy functions
 
 struct _Stack {
-    // TODO: define stack representation
     char** elements; // pointer to an array of strings, so do char** since a pointer to an array of strings which is char*
     int top;
     int capacity;
@@ -16,18 +17,14 @@ stack_response create() {
 }
 
 int size(const stack s) {
-    // top will always be the same thing as the size
-    // stacks are always pointers, so can find the value of what it points to like this. alternative that means same thing is (*s).top
     return s->top;
 }
 
 bool is_empty(const stack s) {
-    // size is 0 and top is 0
     return size(s) == 0;
 }
 
 bool is_full(const stack s) { 
-    // TODO: return top is at MAX_CAPACITY
     return s->top == MAX_CAPACITY;
 }
 
@@ -37,6 +34,14 @@ string_response pop(stack s);             // Will include a copy of the string
                                           // responsible for freeing it
 
 void destroy(stack* s) { // frees *all* the memory
+    if (!s || is_empty(s)) {
+        return;
+    }
+
+    for (int i = 0; i < (*s)->top; i++) {
+        free((*s)->elements[i]);
+    }
+
     free((*s)->elements);
     free(s);
 }
