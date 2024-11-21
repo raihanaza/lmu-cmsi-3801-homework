7c623c08886e8d67404dc78a5204f6b20dd8f105
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-11-21 10:01:33 -0800

    adding reallocate method and implementing that into pop function, lauren code

--- c/string_stack.c
+++ c/string_stack.c
@@ -5,8 +5,6 @@
 
 #define INITIAL_CAPACITY 16
 
-// Complete your string stack implementation in this file.
-
 struct _Stack {
     char** elements;
     int top;
@@ -22,7 +20,7 @@ static response_code reallocate(stack s, int new_capacity) {
     }
 
     char** new_elements = realloc(s->elements, new_capacity * sizeof(char*));
-    if (!new_elements) {
+    if (new_elements == NULL) {
         return out_of_memory;
     }
 
@@ -61,12 +59,10 @@ int size(const stack s) {
 }
 
 bool is_empty(const stack s) {
-    // TODO - size is 0, top is 0
     return size(s) == 0;
 }
 
 bool is_full(const stack s) {
-    // TODO - return top is at MAX_CAPACITY
     return s->top == MAX_CAPACITY;
 }
 
@@ -107,24 +103,21 @@ string_response pop(stack s) {
             stack_empty, NULL
         };
     }
+    
     char* popped = s->elements[--s->top];
     s->elements[s->top] = NULL;
+
     if(s->top <= s->capacity / 4) {
         int new_capacity = s->capacity / 2;
 
-        if (new_capacity < INITIAL_CAPACITY) {
-            new_capacity = INITIAL_CAPACITY;
-        }
-
-        char** new_elements = realloc(s->elements, new_capacity * sizeof(char*));
-        if (new_elements == NULL) {
+        response_code result = reallocate(s,  new_capacity);
+        if (result != success) {
             return (string_response) {
                 out_of_memory, NULL
             };
         }
-        s->elements = new_elements;
-        s->capacity = new_capacity;
     }
+
     return (string_response) {
         success, popped
     };
