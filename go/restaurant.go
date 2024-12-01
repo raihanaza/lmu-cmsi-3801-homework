e97482a5df537014e4d0da0d557325944d4a72ed
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-11-30 17:45:01 -0800

    adding only completed order function

--- go/restaurant.go
+++ go/restaurant.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"math/rand"
-	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -29,43 +28,4 @@ func NewOrder(customer string) *Order {
         customer: customer,
         reply: make(chan *Order),
     }
-}
-
-func Cook(name string) {
-    log.Println(name, "starting work")
-    for {
-        order := <-Waiter
-        do(10, name, "cooking order", order.id, "for", order.customer)
-        order.preparedBy = name
-        order.reply <- order
-    }
-}
-
-func Customer(name string, wg *sync.WaitGroup) {
-    do(10, name, "is eating")
-    for mealsEaten := 0; mealsEaten < 5; {
-        // place an order 
-        // select statement so that if the waiter gets it within 7 seconds
-            // then you get it from the cook, and eat it (mealsEaten++)
-            // otherwise, if you don't get it, leave the restaurant 
-            // do(5, name, "is waiting too long, abandoning the order", order.id)
-    }
-}
-
-func main() {
-    customers := [10]string{
-        "Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai",
-    }
-    // in a loop, start each customer as a goroutine
-    var wg sync.WaitGroup
-    for _, customer := range customers {
-        wg.Add(1)
-        go Customer(customer, &wg)
-    }
-    // start 3 cooks, Remy, Linguini, and Collette 
-    go Cook("Remy")
-    go Cook("Linguini")
-    go Cook("Collette")
-    // wait for all customers to finish 
-    log.Println("Restaurant closing")
 }
