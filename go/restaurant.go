83e5a249b72001e8e359e7079cb8c629567d0eaa
raihanaza <86710982+raihanaza@users.noreply.github.com>
2024-11-30 17:19:52 -0800

    following toal hw helper

--- go/restaurant.go
+++ go/restaurant.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"math/rand"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -17,5 +19,56 @@ func do(seconds int, action ...any) {
     time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
-// Implement the rest of the simulation here. You may need to add more imports
-// above.
+
+// its not bools, its actully Orders
+// not actually correct -- look at the Java
+type Order struct {
+    id uint64
+    customer string
+    // make a reply which is a channel that can take the order (HINT you need a pointer to the order)
+    // also the name of the cook 
+}
+// you also need a mechanism to generate the next order id - similar to java, you need atomic -- import atomic
+var nextId atomic.Uint64 
+
+// a waiter can only hold 3 orders at once
+var Waiter = make(chan *Order, 3)
+
+func Cook(name string) {
+    do(10, name, "is cooking")
+    // log that the cook is starting
+    // loop forever
+    //      wait for an order from the waiter
+    //      cook it 
+    //      put the name of the cook in the order 
+    //      send it back into the reply channel: order.reply <- order
+}
+
+func Customer(name string, wg *sync.WaitGroup) {
+    do(10, name, "is eating")
+    for mealsEaten := 0; mealsEaten < 5; {
+        // place an order 
+        // select statement so that if the waiter gets it within 7 seconds
+            // then you get it from the cook, and eat it (mealsEaten++)
+            // otherwise, if you don't get it, leave the restaurant 
+            // do(5, name, "is waiting too long, abandoning the order", order.id)
+    }
+}
+
+func main() {
+    customers := [10]string{
+        "Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai",
+    }
+    // in a loop, start each customer as a goroutine
+    var wg sync.WaitGroup
+    for _, customer := range customers {
+        wg.Add(1)
+        go Customer(customer, &wg)
+    }
+    // start 3 cooks, Remy, Linguini, and Collette 
+    go Cook("Remy")
+    go Cook("Linguini")
+    go Cook("Collette")
+    // wait for all customers to finish 
+    log.Println("Restaurant closing")
+}
