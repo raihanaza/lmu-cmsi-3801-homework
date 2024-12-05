607b1b8ebe3aa0bc3aee1880e21cda3d239cc04e
ceciliazaragoza <czarago3@lion.lmu.edu>
2024-12-04 23:19:36 -0800

    finished customer method, but still need to make sure that it works with other functions

--- go/restaurant.go
+++ go/restaurant.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -19,3 +20,38 @@ func do(seconds int, action ...any) {
 
 // Implement the rest of the simulation here. You may need to add more imports
 // above.
+type Order struct {
+    id uint64
+    customer string
+    reply chan *Order
+    preparedBy string
+}
+var nextId uint64 
+var Waiter = make(chan *Order, 3)
+
+func NewOrder(customer string) *Order {
+    return &Order{
+        id: atomic.AddUint64(&nextId, 1),
+        customer: customer,
+        reply: make(chan *Order),
+    }
+}
+
+func customer(name string, waitGroup *sync.WaitGroup) {	
+	defer waitGroup.Done()
+	waitGroup.Add(1)
+
+	for mealsEaten := 0; mealsEaten < 5; {
+		order := NewOrder(name)
+		log.Println(name, " placed order ", order.id)
+		select {
+		case Waiter <- order:
+			meal := <- order.reply
+			do(2, name, "eating cooked order", meal.id, "prepared by", meal.preparedBy)
+			mealsEaten++
+		case <- time.After(7):
+			do(5, name, "waiting too long, abandoing order", order.id)
+		}
+	}
+	log.Println(name, " going home")
+}
